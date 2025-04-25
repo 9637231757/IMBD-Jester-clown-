b59fd11610914256a749913a8a9b9b65efa7034a
9637231757 <er.rohangaidhani@gmail.com>
2025-04-25 20:19:45 +0530

    added update function in views.py

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+#from watchlist_app.models import Movie
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -8,4 +10,13 @@ class MovieSerializer(serializers.Serializer):
     
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('descripton', instance.description)
+        instance.active = validated_data.get('Active', instance.Active)
+        instance.save()
+        return instance 
+        
+        
         

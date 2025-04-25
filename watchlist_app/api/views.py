75c4bb4746b2c35dd080915363385ccfec2abe82
9637231757 <er.rohangaidhani@gmail.com>
2025-04-25 18:32:23 +0530

    decorator for GET request

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -4,11 +4,20 @@ from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
 
-@api_view()
+@api_view(['GET'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)        
 
 @api_view()
 def movie_details(request, pk):

fb17e9d4cbd611453af9b23134cd0d1261fa5b3f
9637231757 <er.rohangaidhani@gmail.com>
2025-04-26 23:39:51 +0530

    comment added

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 #from watchlist_app.models import Movie
 
 
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
@@ -11,6 +12,7 @@ class MovieSerializer(serializers.Serializer):
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
     
+    # to update instance update data
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('descripton', instance.description)

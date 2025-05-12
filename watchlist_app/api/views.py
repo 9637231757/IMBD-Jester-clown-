622ff6552d0bd683155acb5bf9bd42c8b098c746
9637231757 <er.rohangaidhani@gmail.com>
2025-05-12 22:30:01 +0530

    views.py

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,4 +1,5 @@
 # views.py function based views
+# function based views.
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view

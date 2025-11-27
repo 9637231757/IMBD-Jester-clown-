ed4735fde6f5c900cda5d5ea900920d2abc72518
Rohan Gaidhani <er.rohangaidhani@gmail.com>
2025-11-27 17:50:07 +0530

    updated user_review

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator 
+from django.contrib.auth.models import User
 
 # Create your models in models.py
 class StreamPlatform(models.Model):
@@ -21,6 +22,7 @@ class WatchList(models.Model):
         return self.title
     
 class Review(models.Model):
+    review_user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     active = models.BooleanField(default=True)
     descriptions = models.CharField(max_length=200, null=True)

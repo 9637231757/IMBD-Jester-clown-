a6179f5d4f4d5290d835a24a6be41337beaee71a
Rohan Gaidhani <er.rohangaidhani@gmail.com>
2025-12-20 15:24:00 +0530

    TokenAuthentication updated

--- watchmate/user_app/api/urls.py
+++ watchmate/user_app/api/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    
+    #path('admin/', admin.site.urls),
     path('login/', obtain_auth_token, name='login'),
+    #path('api/token/', obtain_auth_token),
+
     
  
 ]

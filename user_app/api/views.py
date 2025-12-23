dcd02f77785a92f0352f748cd7ddba325350f2ec
Rohan Gaidhani <er.rohangaidhani@gmail.com>
2025-12-23 18:09:14 +0530

    auto token generation implemented

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,6 +2,8 @@
 from rest_framework.decorators import api_view 
 from rest_framework.response import Response
 from user_app.api.serializers import RegistrationSerializer 
+from rest_framework.authtoken.models import Token 
+from user_app import models
 
 
 # Create your views here.
@@ -10,9 +12,23 @@ def registration_view(request):
     
     if request.method == 'POST':
          serializer = RegistrationSerializer(data=request.data)
+         
+         data = {}
+         
          if serializer.is_valid():
-             serializer.save() 
-             return Response(serializer.data) 
+             account = serializer.save() 
+             
+             data['response'] = "Regestration Sucessful!"
+             data['username'] = account.username 
+             data['email'] = account.email 
+             
+             token = Token.objects.get(user=account).key 
+             data['token'] = token 
+             
+         else: 
+             data = serializer.errors
+                 
+         return Response(data) 
          
     
         

b698089e0c7b893b55f90228a95cf0c2f0db576f
Rohan Gaidhani <er.rohangaidhani@gmail.com>
2025-12-28 17:18:49 +0530

    jwt implemented

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import registration_view, logout_view
  
 urlpatterns = [
     
     #path('admin/', admin.site.urls),
-    path('login/', obtain_auth_token, name='login'),
+    #path('login/', obtain_auth_token, name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('register/',registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
-    path('api/token/', obtain_auth_token),
+    #path('api/token/', obtain_auth_token),
+    
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     
 ]

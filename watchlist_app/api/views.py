c23d17b3719bd71f05ab9d18b4d10cb80e09aaee
Rohan Gaidhani <er.rohangaidhani@gmail.com>
2025-12-30 16:52:31 +0530

    userratethrottling implemented in views

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -56,6 +56,7 @@ class ReviewList(generics.ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     #permission_classes = [IsAuthenticated]
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
     
     
     def get_queryset(self):
